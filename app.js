7e6a9a2a3db437c64ff542de9a545709eae6028e
louislong <louislong@me.com>
2017-09-26 18:51:09 +0800

    [refactor-code] refactor code, extract the blocks and use as a global value

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 var express = require('express');
 var app = express();
 
+var blocks = {
+  fixed: 'fasten securely in position',
+  movable: 'capable of being moved',
+  rotating: 'move in a circle around its center'
+}
+
 // use app.param() to intercept request
 // convert all the letters the param 'name' to lower case
 app.param('name', (req, res, next) => {
@@ -12,21 +18,16 @@ app.param('name', (req, res, next) => {
 
 // route with query string
 app.get('/blocks', (req, res) => {
-  var blocks = ['fixed', 'movable', 'rotating'];
+  var blocksNames = Object.keys(blocks);
   if (req.query.limit > 0) {
-    res.json(blocks.slice(0, req.query.limit))
+    res.json(blocksNames.slice(0, req.query.limit));
   } else {
-    res.json(blocks);
+    res.json(blocksNames);
   }
 })
 
 // dynamic route with params
 app.get('/blocks/:name', (req, res) => {
-  var blocks = {
-    fixed: 'fasten securely in position',
-    movable: 'capable of being moved',
-    rotating: 'move in a circle around its center'
-  }
 
   // handle path not found case
   var desc = blocks[req.params.name];
