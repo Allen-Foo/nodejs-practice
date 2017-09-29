46e7948961c8f3aee24d66b8a8dbcb1d4e5d0ec9
Allen-Foo <fuyu0878@gmail.com>
2017-09-29 20:15:11 +0800

    [todo] add todo router, use mongoose to connect local Database
    -- implement post and get route
    -- use local database 'todoDB'

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // app.js
 
+// set up ========================
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');               
 
 var blocksRouter = require('./routes/blocks');
+var todosRouter = require('./routes/todos');
 
+
+// configuration =================
+mongoose.connect("mongodb://localhost:27017/todoDB")
 // router is mounted in a particular root url
 // only requests to /blocks/* will be sent to our "router"
 app.use('/blocks', blocksRouter);
-
+app.use('/todos', todosRouter);
 
 // start the server on port 3000
 app.listen(3000);
---
+++ routes/todos.js
@@ -0,0 +1,46 @@
+// todo.js
+var mongoose = require('mongoose');
+
+var express = require('express');
+var router = express.Router();
+
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
+
+// define model by mongoose
+var Todo = mongoose.model('Todo', {
+  text: String,
+  completed: Boolean
+})
+
+
+// routes for todo
+router.route('/')
+  .get((req, res) => {
+    // use mongoose to get all todos
+    Todo.find((err, todos) => {
+      if(err)
+        res.send(err);
+
+      res.json(todos);
+    })
+  })
+  .post(parseUrlencoded, (req, res) => {
+    console.log('req', req.body);
+    Todo.create({
+      text: req.body.text,
+      completed: false
+    }, (err, todos) => {
+      if (err)
+        res.send(err);
+
+      // get all the todos and send back 
+      Todo.find((error, todos) => {
+        if (error)
+          res.send(error);
+        res.json(todos)
+      })
+    })
+  })
+
+  module.exports = router
