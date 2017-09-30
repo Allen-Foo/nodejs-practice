23dd8a6280d0db6aa6763c4331456de004a7154d
Allen-Foo <fuyu0878@gmail.com>
2017-09-30 18:12:42 +0800

    [api-doc] add api doc for route 'todos'

--- routes/todos.js
+++ routes/todos.js
@@ -26,10 +26,60 @@ const returnAllTodos = (res) => {
 
 // routes for todo
 router.route('/')
+  /**
+   * @api {get} /todos Get all the todos
+   * @apiName GetTodos
+   * @apiGroup Todos
+   *
+   * @apiSuccess {Array} todos The array of todos
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "_id": "59ce380340e5e152b5126762",
+   *         "text": "test",
+   *         "completed": false
+   *       },
+   *       {
+   *         "_id": "59ce383840e5e152b5126763",
+   *         "text": "allen",
+   *         "completed": true
+   *       },
+   *     ]
+   */
   .get((req, res) => {
     // use mongoose to get all todos
     returnAllTodos(res)
   })
+  /**
+   * @api {post} /todos Create new todo
+   * @apiName CreateNewTodo
+   * @apiGroup Todos
+   *
+   * @apiParam {String} text the name of the new todo
+   * @apiParamExample {json} param example
+   * {
+   *   "text": "this is for test"
+   * }
+   *
+   * @apiSuccess {Array} todos The array of todos
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "_id": "59ce380340e5e152b5126762",
+   *         "text": "test",
+   *         "completed": false
+   *       },
+   *       {
+   *         "_id": "59ce383840e5e152b5126763",
+   *         "text": "allen",
+   *         "completed": true
+   *       },
+   *     ]
+   */
   .post(parseUrlencoded, (req, res) => {
     console.log('req', req.body);
     Todo.create({
@@ -44,6 +94,34 @@ router.route('/')
   })
 
 router.route('/:todo_id')
+  /**
+   * @api {put} /todos/:todo_id Update the status of todo
+   * @apiName UpdateTodos
+   * @apiGroup Todos
+   *
+   * @apiParam {String} id The todo id    
+   * @apiParamExample {json} param example
+   *  {
+   *    "todo_id": "59ce380340e5e152b5126762"
+   *  }
+   *
+   * @apiSuccess {Array} todos The array of todos
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "_id": "59ce380340e5e152b5126762",
+   *         "text": "test",
+   *         "completed": false
+   *       },
+   *       {
+   *         "_id": "59ce383840e5e152b5126763",
+   *         "text": "allen",
+   *         "completed": true
+   *       },
+   *     ]
+   */
   .put((req, res) => {
     Todo.where({ _id: req.params.todo_id})
       .update({text: req.body.text, completed: req.body.completed})
@@ -54,6 +132,34 @@ router.route('/:todo_id')
         returnAllTodos(res)
       })
   })
+  /**
+   * @api {delete} /todos/:todo_id Delete a todo item
+   * @apiName DeleteTodo
+   * @apiGroup Todos
+   *
+   * @apiParam {String} id The todo id    
+   * @apiParamExample {json} param example
+   *  {
+   *    "todo_id": "59ce380340e5e152b5126762"
+   *  }
+   *  
+   * @apiSuccess {Array} todos The array of todos
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "_id": "59ce380340e5e152b5126762",
+   *         "text": "test",
+   *         "completed": false
+   *       },
+   *       {
+   *         "_id": "59ce383840e5e152b5126763",
+   *         "text": "allen",
+   *         "completed": true
+   *       },
+   *     ]
+   */
   .delete((req, res) => {
     Todo.remove({
       _id: req.params.todo_id
